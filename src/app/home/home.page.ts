ff6026a13c5e8eed634f5183a7335f0aebfe1a86
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-06-11 03:11:16 +0200

    App funktioniert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+import { PalindromService } from '../palindrom.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public eingabe : string = "";
+
+  /**
+   * Konstruktor für Dependency Injection.
+   */
+  constructor(private palindromService: PalindromService,
+              private alertCtrl       : AlertController) {}
+
+
+  /**
+   * Button-Event-Handler für Palindrom-Check.
+   */
+  public async onPalindromCheck() {
+
+    const eingabeTrimmed = this.eingabe.trim();
+    if (eingabeTrimmed.length === 0) {
+
+      this.zeigeDialog("Ungültige Eingabe", "Nichts eingegeben");
+      return;
+    }
+
+    const istPalindrom = this.palindromService.checkePalindrom(eingabeTrimmed);
+    if (istPalindrom) {
+
+      this.zeigeDialog("Ergebnis", `"${eingabeTrimmed}" ist ein Palindrom`);
+
+    } else {
+
+      this.zeigeDialog("Ergebnis", `"${eingabeTrimmed}" ist kein Palindrom`);
+    }
+  }
+
+
+  /**
+   * Hilfsmethode um Dialog mit Titel anzuzeigen.
+   * 
+   * @param titel Dialog-Titel
+   * @param nachricht Im Dialog anzuzeigende Meldung.
+   */
+  async zeigeDialog(titel: string, nachricht: string) {
+
+    const meinAlert =
+          await this.alertCtrl.create({
+              header  : titel,
+              message : nachricht,
+              buttons : [ "Ok" ]
+          });
+
+    await meinAlert.present();
+  }
 
 }
---
+++ src/app/palindrom.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PalindromService {
+
+  /**
+   * Als Argument übergebener String auf Palindrom-Eigenschaft überprüfen.
+   * 
+   * @param eingabe String, der auf Palindrom-Eigenschaft zu überprüfen ist.
+   * @returns `true` gdw. `eingabe` ein Palindrom ist
+   */
+  public checkePalindrom(eingabe: string): boolean {
+
+    const eingabeNormalisiert = eingabe.trim().toLowerCase();
+    const eingabeLaenge = eingabeNormalisiert.length;
+
+    if (eingabeLaenge === 0) { return false; }
+    if (eingabeLaenge === 1) { return true; }
+
+    const anzahlChecks = Math.floor( eingabeLaenge / 2 );
+
+    for (let i = 0; i < anzahlChecks; i++) {
+
+      let index1 = i;
+      let index2 = eingabeLaenge - i - 1;
+
+      let a = eingabeNormalisiert.charAt( index1 );
+      let b = eingabeNormalisiert.charAt( index2 );
+
+      if (a !== b) {
+
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+}
