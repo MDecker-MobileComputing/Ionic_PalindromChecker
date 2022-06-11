ffda08eeec9c0e65bf1842058c1280e240d0f327
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-06-11 11:16:28 +0200

    spec.ts-Datei weiter vorbereitet

--- src/app/palindrom.service.spec.ts
+++ src/app/palindrom.service.spec.ts
@@ -3,18 +3,29 @@ import { TestBed } from '@angular/core/testing';
 import { PalindromService } from './palindrom.service';
 
 
+/**
+ * Rechnerübung: Schreiben Sie Unit-Test-Methoden in dieser Klasse.
+ */
 describe('PalindromService', () => {
 
-  /** Code/Class under Test */
+  /** Code/Class under Test (CUT). */
   let cut: PalindromService;
 
 
+  /**
+   * Diese Methode wird vor JEDER einzelnen Unit-Test-Methode aufgerufen,
+   * damit jede Testmethode eine "frische" Instanz des CUT hat.
+   */
   beforeEach(() => {
 
     TestBed.configureTestingModule({});
     cut = TestBed.inject(PalindromService);
   });
 
+
+  /**
+   * Vorlage für eine Unit-Test-Methode.
+   */
   it('should be created', () => {
 
     expect(cut).toBeTruthy();
